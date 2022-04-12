85b5604e54a1a9c4a4767f149728a7e0b466577d
tonynnabs <tonynnabs01@gmail.com>
2022-04-12 19:26:53 -0400

    saving cart to db

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -5,6 +6,13 @@ import Products from './components/Shop/Products';
 
 function App() {
   const cartisVisible = useSelector(state => state.ui.isVisible)
+  const cart = useSelector(state => state.cart);
+  useEffect(()=> {
+    fetch('https://react-http-request-c314b-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    });
+  },[cart]);
   return (
     <Layout>
       {cartisVisible && <Cart />}
